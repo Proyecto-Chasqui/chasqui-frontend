28c97004e7739c6984638168df5093489fc44374
Juan <juan.93.ctm@gmail.com>
2018-02-14 15:01:42 -0300

    Eliminado contextPurchaseService de HomeCtrl

--- src/app/comercio/principal/home/home.ctrl.js
+++ src/app/comercio/principal/home/home.ctrl.js
@@ -6,7 +6,7 @@
 		.controller('HomeController', HomeController);
 
     
-	function HomeController($scope, sellerService, CTE_REST, catalogs_dao, $state, contextoCompraService) {
+	function HomeController($scope, sellerService, CTE_REST, catalogs_dao, $state) {
         
         $scope.catalogs = catalogs_dao.getCatalogs();
         
