4607a3bae3998d03a3cdd5bff46154540b29270b
Juan <juan.93.ctm@gmail.com>
2018-02-24 19:59:29 -0300

    Revert "Adaptación a un nuevo concepto: los catálogos son almacenados en el localStorage y son accedidos a traves del catalogs_dao"
    This reverts commit 9d818b76bae3eac17f8a85dc4f4f7f06a0b65242.

--- src/app/comercio/principal/menus/catalog/items/catalog-menu-items.ctrl.js
+++ src/app/comercio/principal/menus/catalog/items/catalog-menu-items.ctrl.js
@@ -6,21 +6,15 @@
 		.controller('CatalogMenuItemsController', CatalogMenuItemsController);
 
     
-	function CatalogMenuItemsController($scope, $stateParams, itemsBuilder, navigation_state, usuario_dao, catalogs_dao) {
+	function CatalogMenuItemsController($scope, globalConfigurations, navigation_state, usuario_dao) {
        
-        $scope.catalog = catalogs_dao.getCatalog($stateParams.id);
-        
         $scope.isLogued = usuario_dao.isLogged();
         
         $scope.classFor = function(page) {
 			return (navigation_state.getSelectedTab() == page)?"md-accent":"";
 		}
            
-        $scope.menuItems = itemsBuilder($scope.catalog.estrategia.few);
-        
-        $scope.toTop = function(){
-            window.scrollTo(0,0);
-        }
+        $scope.menuItems = globalConfigurations[$scope.catalog.id].menus;
         
 	}
 })();
--- src/app/comercio/principal/menus/catalog/items/catalog-menu-items.directive.js
+++ src/app/comercio/principal/menus/catalog/items/catalog-menu-items.directive.js
@@ -6,6 +6,7 @@ angular.module('chasqui').directive('catalogMenuItems', [
         restrict: 'E',
         controller: 'CatalogMenuItemsController',
         scope: {
+            catalog: '=catalog'
         },
         templateUrl: 'app/comercio/principal/menus/catalog/items/catalog-menu-items.tmpl.html'
       };
