4fd79e0d350b7da7444d4c749d3b18de1d6f9ddb
Juan <juan.93.ctm@gmail.com>
2018-02-28 16:42:51 -0300

    Adaptada la interfaz de context-orders.service a la de orders_dao

--- src/app/persistencia/orders_dao.js
+++ src/app/persistencia/orders_dao.js
@@ -53,6 +53,7 @@ function orders_dao(catalogs_data){
         }, []);
     }
     
+    
     function getOrdersByType(catalogId, ordersType){
         return catalogs_data.getCatalog(catalogId)[ordersType];
     }
