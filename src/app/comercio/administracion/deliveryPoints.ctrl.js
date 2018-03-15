519c1c21f9789f987375953818dbbcae947b31a3
Juan <juan.93.ctm@gmail.com>
2018-03-15 16:07:50 -0300

    CARD:https://trello.com/c/6wr97IVl/678-vista-para-puntos-de-entrega

--- src/app/comercio/administracion/deliveryPoints.ctrl.js
+++ src/app/comercio/administracion/deliveryPoints.ctrl.js
@@ -3,9 +3,60 @@
     
     angular.module('chasqui').controller('DeliveryPointsCtrl', DeliveryPointsCtrl);
     
-	function DeliveryPointsCtrl(navigation_state) {
+	function DeliveryPointsCtrl(navigation_state, $scope) {
         
         navigation_state.goDeliveryPointsTab();
+        
+        $scope.formatAddress = formatAddress;
+        
+        $scope.deliveryPoints = [{
+                    id: 1,
+                    nombre: "Hirigoyen",
+                    mensaje: "La próxima fecha de entrega va a ser el 31/2",
+                    direccion: {
+                        id: 7,
+                        calle: "av Hirigoyen",
+                        calleAdyacente1: null,
+                        calleAdyacente2: null,
+                        altura: 270,
+                        localidad: "Quilmes",
+                        codigoPostal: "",
+                        alias: null,
+                        departamento: null,
+                        latitud: null,
+                        longitud: null,
+                        predeterminada: null,
+                        geoUbicacion: null,
+                        comentario: "algo"
+                    }
+                }, {
+                    id: 2,
+                    nombre: "Beltran",
+                    mensaje: "La próxima fecha de entrega va a ser el 31/2",
+                    direccion: {
+                        id: 8,
+                        calle: "beltran",
+                        calleAdyacente1: null,
+                        calleAdyacente2: null,
+                        altura: 180,
+                        localidad: "Bernal",
+                        codigoPostal: "",
+                        alias: null,
+                        departamento: null,
+                        latitud: null,
+                        longitud: null,
+                        predeterminada: null,
+                        geoUbicacion: null,
+                        comentario: "algo"
+                    }
+                }
+            ];
+        
+        ///////////////////////////////
+        
+        function formatAddress(address){
+            return address.calle + " " + address.altura;
+        }
         
     }
     
