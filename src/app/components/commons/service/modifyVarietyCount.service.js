7db05241a47f9b48cf260ca4a5d54f69d984921c
Juan <juan.93.ctm@gmail.com>
2018-04-06 14:20:05 -0300

    Cableada la forma de agregar productos al pedido individual

--- src/app/components/commons/service/modifyVarietyCount.service.js
+++ src/app/components/commons/service/modifyVarietyCount.service.js
@@ -1,13 +1,18 @@
 (function() {
 	'use strict';
 
-	angular.module('chasqui').service('ModifyVarietyCount', ModifyVarietyCount);
+	angular.module('chasqui').service('modifyVarietyCount', modifyVarietyCount);
 
-	function ModifyVarietyCount($log, dialogCommons, contextPurchaseService, ToastCommons, $rootScope, productoService, us, $state){
+	function modifyVarietyCount($log, dialogCommons, contextPurchaseService, ToastCommons, $rootScope, 
+                                 productoService, us, $state, contextOrdersService){
 
-        this.modifyDialog = function(variety){
-                        
-            console.log(variety);
+        return {
+            modifyDialog: modifyDialog
+        }
+        
+        ///////////////////////////////////////////////////////////////////////
+        
+        function modifyDialog(variety){
             var varietyName = (variety.nombreProducto === undefined)? variety.nombre : variety.nombreProducto;
             
             var initialCount = initialCountForVariety(variety);
@@ -22,10 +27,9 @@
             }
 
             function doNoOk() {
-                $log.debug("Cancelo Agregar")
+                $log.debug("Cancelo Agreglar")
             }
 
-            //dialogCommons.prompt('Agregar al changuito', '¿Cuántos ' + variety.nombreProducto + ' necesitas?','Cantidad', 'Agregar', 'Cancelar', doOk, doNoOk);
             dialogCommons.modifyVarietyCount({
                                                 title: "¿Cuántos " + varietyName + " necesitas?",
                                                 okButton: "Agregar al carrito",
@@ -41,18 +45,33 @@
         /* Private */
         
 		function callAddToCart(variety, count) {
-            modifyVarietyCount(variety, count, productoService.agregarPedidoIndividual, 'PRODUCTO_AGREGADO');
+            modifyVarietyCount(variety, count, 1, productoService.agregarPedidoIndividual, 'PRODUCTO_AGREGADO');
 		}
         
         function callRemoveFromCart(variety, count){
-            modifyVarietyCount(variety, count, productoService.quitarProductoIndividual, 'QUITO_PRODUCTO');
+            modifyVarietyCount(variety, count, -1, productoService.quitarProductoIndividual, 'QUITO_PRODUCTO');
         }
         
-        function modifyVarietyCount(variety, count, modifierFunction, modifierOkText){
+        
+        function modifyVarietyCount(variety, count, sign, modifierFunction, modifierOkText){
             function doOk(response) {
 				ToastCommons.mensaje(us.translate(modifierOkText));
-				contextPurchaseService.refreshPedidos();
 				$rootScope.$emit('lista-producto-agrego-producto');
+                
+                function orderModification(order){
+                    if(order.productosResponse.filter(function(p){return p.idVariante == variety.idVariante}).length > 0){
+                        var index = order.productosResponse.map(function(p){return p.idVariante}).indexOf(variety.idVariante);
+                        order.productosResponse[index].cantidad += (sign * count);
+                    }else{
+                        order.productosResponse.push(formatLikeServerVariety(variety, count));
+                    }
+                    
+                    return order;
+                }
+                
+                contextOrdersService.modifyOrder(contextPurchaseService.getCatalogContext(),
+                                                 contextPurchaseService.getSelectedOrder(),
+                                                 orderModification);
 			}
             
 			var params = {
@@ -68,6 +87,16 @@
         function initialCountForVariety(variety){
             var varietyInOrder = contextPurchaseService.getSelectedOrder().productosResponse.filter(function(p){return p.idVariante === variety.idVariante});
             return (varietyInOrder.length === 1)? varietyInOrder[0].cantidad : 0;
+        }
+        
+        function formatLikeServerVariety(variety, count){
+            return {
+                nombre: variety.nombreProducto,
+                idVariante: variety.idVariante,
+                precio: variety.precio,
+                cantidad: count,
+                imagen: variety.imagenPrincipal
+            }
         }
 	} 
 })(); 
