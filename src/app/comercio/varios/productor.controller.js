b5a43e942b189dfb3a7821a17850928d97816552
Juan <juan.93.ctm@gmail.com>
2018-05-18 15:46:56 -0300

    Arreglada la pagina de Productor

--- src/app/comercio/varios/productor.controller.js
+++ src/app/comercio/varios/productor.controller.js
@@ -6,11 +6,10 @@
 		.controller('ProductorController', ProductorController);
 
 	/** @ngInject */
-	function ProductorController($log, $stateParams, $scope, URLS, REST_ROUTES, navigation_state, 
+	function ProductorController($log, $stateParams, $scope, URLS, REST_ROUTES, 
                                   productorService, ToastCommons, us, contextPurchaseService, contextCatalogObserver) {
 		
         $log.debug('EmprenController ..... ');
-		//navigation_state.goMakersTab();
         
 
 		$scope.urlBase = URLS.be_base;
@@ -29,10 +28,11 @@
             contextCatalogObserver.observe(function(){
                 productorService.getProductores()
                     .then(function(response) { 
-                        var idProductor = contextPurchaseService.getCatalogContext();
+                        var idProductor = $stateParams.idProductor;
                         $scope.productores = response.data; 
                         console.log($scope.productores, idProductor);
                         $scope.productor = $scope.productores.filter(function(p){ return p.idProductor === parseInt(idProductor);})[0];
+                        console.log(idProductor, $scope.productor);
                 })
             })
 		}
