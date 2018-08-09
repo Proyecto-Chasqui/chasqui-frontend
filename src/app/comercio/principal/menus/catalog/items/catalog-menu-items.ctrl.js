5a3bbaf09a5c8f81b227023a8ba6de631fcad59e
Juan <juan.93.ctm@gmail.com>
2018-08-09 15:25:03 -0300

    Cambiados tabs por espacios

--- src/app/comercio/principal/menus/catalog/items/catalog-menu-items.ctrl.js
+++ src/app/comercio/principal/menus/catalog/items/catalog-menu-items.ctrl.js
@@ -9,39 +9,39 @@
 	function CatalogMenuItemsController($scope, $stateParams, itemsBuilder, navigation_state, usuario_dao, contextCatalogsService) {
        
         
-        $scope.catalog;
-        $scope.isLogued;
-        $scope.menuItems;
-        $scope.classFor = classFor;
-        $scope.toTop = toTop;
-        
-        function init(){
-            $scope.isLogued = usuario_dao.isLogged();
-            contextCatalogsService.getCatalogByShortName($stateParams.catalogShortName).then(function(catalog){
-                $scope.catalog = catalog;
-                $scope.menuItems = itemsBuilder($scope.catalog.few);
-            })
-        }
-        
-        
-        //////////////// Implementations
-        
-        function classFor(page) {
-			return (navigation_state.getSelectedTab() == page)?"md-accent":"";
-		}           
-        
-        function toTop(){
-            window.scrollTo(0,0);
-        }
-        
-        $scope.$on('logout', function(event, msg) {
-			init();
-		});
-        
-        $scope.$on('resetHeader', function(event, msg) {
-			init();
-		});
-        
+    $scope.catalog;
+    $scope.isLogued;
+    $scope.menuItems;
+    $scope.classFor = classFor;
+    $scope.toTop = toTop;
+
+    function init(){
+        $scope.isLogued = usuario_dao.isLogged();
+        contextCatalogsService.getCatalogByShortName($stateParams.catalogShortName).then(function(catalog){
+            $scope.catalog = catalog;
+            $scope.menuItems = itemsBuilder($scope.catalog.few);
+        })
+    }
+
+
+    //////////////// Implementations
+
+    function classFor(page) {
+        return (navigation_state.getSelectedTab() == page)?"md-accent":"";
+    }           
+
+    function toTop(){
+        window.scrollTo(0,0);
+    }
+
+    $scope.$on('logout', function(event, msg) {
         init();
+    });
+
+    $scope.$on('resetHeader', function(event, msg) {
+        init();
+    });
+
+    init();
 	}
 })();
