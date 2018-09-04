1f764c641ecebeccfade60cdf4446e6e4f5662cd
Juan <juan.93.ctm@gmail.com>
2018-09-04 17:20:46 -0300

    Fix identation

--- src/app/comercio/catalogo/contexto.compra.controller.js
+++ src/app/comercio/catalogo/contexto.compra.controller.js
@@ -1,60 +1,65 @@
 (function() {
-	'use strict';
+  'use strict';
 
-	angular
-		.module('chasqui')
-		.controller('ContextoCompraController', ContextoCompraController);
+  angular
+    .module('chasqui')
+    .controller('ContextoCompraController', ContextoCompraController);
 
-	/**
-	 * Lista lateral de productos del pedido seleccionado
-	 */
-	function ContextoCompraController($rootScope, $log, $scope, contextPurchaseService, agrupationTypeVAL,
+  /**
+   * Lista lateral de productos del pedido seleccionado
+   */
+  function ContextoCompraController($rootScope, $log, $scope, contextPurchaseService, agrupationTypeVAL,
                                        usuario_dao, contextAgrupationsService, order_context, contextCatalogObserver) {
 
-		$log.debug("ContextoCompraController ..... ");
+    $log.debug("ContextoCompraController ..... ");
 
-        $scope.grupos = [];
-        $scope.agrupationSelected = {};
-        $scope.cambiarContexto = cambiarContexto;
-        $scope.showSelector = showSelector;
-        $scope.setGroupAsAgrupationSelected = setGroupAsAgrupationSelected;
-        $scope.setPersonalAsAgrupationSelected = setPersonalAsAgrupationSelected;
-        
-        function init(){
-            contextCatalogObserver.observe(function(){
-                $scope.agrupationSelected = {
-                    idGrupo: contextPurchaseService.getAgrupationContextId(),
-                    type: agrupationTypeVAL.TYPE_GROUP
-                }
-                contextPurchaseService.getAgrupations().then(function(agrupationsInt) {
-                        $scope.grupos = agrupationsInt.getAgrupationsByType(contextPurchaseService.getCatalogContext(), 
-                                                                            agrupationTypeVAL.TYPE_GROUP);
-                    });
-            })
-        }
-        
-        init();
-        
-        /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    $scope.grupos = [];
+    $scope.agrupationSelected = {};
+    $scope.cambiarContexto = cambiarContexto;
+    $scope.showSelector = showSelector;
+    $scope.setGroupAsAgrupationSelected = setGroupAsAgrupationSelected;
+    $scope.setPersonalAsAgrupationSelected = setPersonalAsAgrupationSelected;
 
-		function cambiarContexto() {
-            console.log("Cambio de contexto:", $scope.agrupationSelected);
-            contextPurchaseService.setContextByAgrupation($scope.agrupationSelected);
-			$rootScope.$emit('contexto.compra.cambia.grupo', $scope.agrupationSelected);
-		}
+    function init(){
+      contextCatalogObserver.observe(function(){
+        $scope.agrupationSelected = {
+          idGrupo: contextPurchaseService.getAgrupationContextId(),
+          type: agrupationTypeVAL.TYPE_GROUP
+        }
+        contextPurchaseService.getAgrupations().then(function(agrupationsInt) {
+          $scope.grupos = agrupationsInt.getAgrupationsByType(contextPurchaseService.getCatalogContext(), 
+                                                              agrupationTypeVAL.TYPE_GROUP);
+        });
+      })
+    }
 
-        function showSelector(){
-            return $scope.grupos.length > 0;
-        }
-        
-        function setGroupAsAgrupationSelected(){
-            $scope.agrupationSelected.type = agrupationTypeVAL.TYPE_GROUP;
-        }
-        
-        function setPersonalAsAgrupationSelected(){
-            $scope.agrupationSelected.type = agrupationTypeVAL.TYPE_PERSONAL;
-        }
+    init();
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    function cambiarContexto() {
+      console.log("Cambio de contexto:", $scope.agrupationSelected);
+      contextPurchaseService.setContextByAgrupation($scope.agrupationSelected);
+      $rootScope.$emit('contexto.compra.cambia.grupo', $scope.agrupationSelected);
+    }
+
+    function showSelector(){
+        return $scope.grupos.length > 0;
+    }
+
+    function setGroupAsAgrupationSelected(){
+        $scope.agrupationSelected.type = agrupationTypeVAL.TYPE_GROUP;
+    }
+
+    function setPersonalAsAgrupationSelected(){
+        $scope.agrupationSelected.type = agrupationTypeVAL.TYPE_PERSONAL;
+    }
+
+    
+    $rootScope.$on('contexto.compra.cambia.grupo', function(event, grupo) {
+      init();
+    });
 
         
-	}
+  }
 })();
