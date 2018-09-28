5970a648e58a72172227b5f498a8f10384340f36
favio <favioc71@live.com.ar>
2018-09-28 06:20:07 -0300

    Modificacion de codigo en paginador para volverlo responsive.

--- src/app/comercio/catalogo/catalogBox/chqCatalogBox.ctrl.js
+++ src/app/comercio/catalogo/catalogBox/chqCatalogBox.ctrl.js
@@ -1,5 +1,10 @@
 (function(){
 	'use strict'
+	/*
+	* Adaptable container with css grid.
+	* Favio Juan.
+	* 9-28-2018
+	*/
 	angular.module('chasqui').controller('ChqCatalogBoxCtrl', ChqCatalogBox);
 
 	function ChqCatalogBox($scope){
