09a648ce492cfa68e8709829a782d65f469b2400
Juan <juan.93.ctm@gmail.com>
2018-12-07 16:23:09 -0300

    Dispatcheado el resumen segun el tipo de pedido

--- src/app/comercio/carrito/detalle-pedido/detalle-pedido.ctrl.js
+++ src/app/comercio/carrito/detalle-pedido/detalle-pedido.ctrl.js
@@ -7,4 +7,13 @@
   function DetallePedidoDialogController($scope, order) {
     $scope.order = order;
     
+    $scope.isPersonal = function(order){
+        return order.idGrupo == null; 
+    }
+
+    $scope.isCollective = function(order){
+        console.log(order);
+        return !$scope.isPersonal(order);
+    }
+    
 }})();
