259d8e0c5322ae873fd55be3d0e5903c0a58292c
Juan <juan.93.ctm@gmail.com>
2019-08-30 16:27:29 -0300

    Fix init

--- src/app/index.run.js
+++ src/app/index.run.js
@@ -6,7 +6,7 @@
 		.run(runBlock);
 
 	/** @ngInject */
-	function runBlock($log, $rootScope, $state, us, usuario_dao) {
+	function runBlock($log, $rootScope, $state, us, usuario_dao, $stateParams) {
 
 		$rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
 
@@ -15,8 +15,18 @@
 
 			if (toState.auth && (!usuario_dao.isLogged())) {
 				$log.debug("ir a logu !!!", toState.name);
-				event.preventDefault();
-				$state.go('catalog.login', { toPage: toState.name })
+        event.preventDefault();
+
+        const catalogShortname = window.location.href.slice(
+          window.location.href.indexOf("#")+2,
+          window.location.href.indexOf("/", window.location.href.indexOf("#")+2),
+        )
+
+        console.log("run", catalogShortname);
+				$state.go('catalog.login', { 
+          catalogShortName: catalogShortname,
+          toPage: toState.name 
+        })
 			}
 
 		});
