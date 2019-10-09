c50bc5d95b5cdda6c2c490df2a42fbac4be2fe2e
Juan <juan.93.ctm@gmail.com>
2019-10-09 15:24:55 -0300

    Arreglada la identación de getContext.js

--- src/app/components/commons/service/context/utils/getContext.js
+++ src/app/components/commons/service/context/utils/getContext.js
@@ -5,24 +5,24 @@
     
 	function getContext($log, $q, REST_ROUTES){
          
-        function cacheTimeExpired(lastUpdate){
+    function cacheTimeExpired(lastUpdate){
 			return parseInt(moment().diff(lastUpdate))/1000 > REST_ROUTES.TIEMPO_MAX_CACHE;
 		}
         
-        // Interfaz 
-        return function (lastUpdate, contextName, context, cacheIsEmpty, returnFromServer){
+    // Interfaz 
+    return function (lastUpdate, contextName, context, cacheIsEmpty, returnFromServer){
 			var defered = $q.defer();
 			var promise = defered.promise;
 
 			if (cacheTimeExpired(lastUpdate) || cacheIsEmpty) {
-                $log.debug("NO tiene " + contextName + " en cache");
-                returnFromServer(defered);
+        $log.debug("NO tiene " + contextName + " en cache");
+        returnFromServer(defered);
 			} else {
 				context().then(defered.resolve);
 			}
 
 			return promise;
 		}
-    }
+  }
      
 })();   
