30784ca2181468e7589cb9ce568998651bf1a2cd
Juan <juan.93.ctm@gmail.com>
2019-10-15 16:13:31 -0300

    Ahora la cache pide los nodos

--- src/app/components/commons/service/context/context-agrupations.service.js
+++ src/app/components/commons/service/context/context-agrupations.service.js
@@ -6,7 +6,7 @@
     
 	function contextAgrupationsService($localStorage, $q, setPromise, getContext, agrupations_dao, moment, gccService, 
                                         ensureContext, idGrupoPedidoIndividual, idPedidoIndividualGrupoPersonal, 
-                                        agrupationTypeVAL, agrupationTypeDispatcher, usuario_dao){
+                                        agrupationTypeVAL, agrupationTypeDispatcher, usuario_dao, nodeService){
      
         
         ///////////////////////////////////////// Interface \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
@@ -43,31 +43,47 @@
 
         
         function getAgrupations(catalogId) {
-            return getContext(
-                vm.ls.lastUpdate,
-                //moment().add(-1.5, 'days'),
-                "grupos", 
-                
-                function(){
-                    var defered = $q.defer();
-                    var promise = defered.promise;
+          // return setPromise(function(defered){
+          //   ensureAgrupations(catalogId, agrupationType).then(function(agrupations){
+          //     defered.resolve(agrupations);
+          //   });
+          // });
+
+          return getContext(
+            vm.ls.lastUpdate,
+            //moment().add(-1.5, 'days'),
+            "todos", 
+            
+            function(){
+                var defered = $q.defer();
+                var promise = defered.promise;
+                defered.resolve(agrupations_dao);
+                return promise;                
+            },                
+            
+            agrupations_dao.getAgrupations(catalogId).length === 0, 
+            
+            function(defered){
+              var agrupations = [grupoIndividualVirtual];
+
+                function doOKGroups(responseGroups) {
+                  var groups = formatAgrupations(responseGroups.data, agrupationTypeVAL.TYPE_GROUP);
+                  agrupations = agrupations.concat(groups);
+
+                  function doOKNodes(responseNodes){
+                    var nodes = formatAgrupations(responseNodes.data, agrupationTypeVAL.TYPE_NODE);
+                    agrupations = agrupations.concat(nodes);  
+  
+                    vm.ls.lastUpdate=moment();	
+                    agrupations_dao.loadAgrupations(catalogId, agrupations);
                     defered.resolve(agrupations_dao);
-                    return promise;                
-                },                
-                
-                agrupations_dao.getAgrupations(catalogId).length === 0, 
-                
-                function(defered){
-                    function doOK(response) {					
-                        vm.ls.lastUpdate=moment();	
-                        var agrupations = formatAgrupations(response.data);
-                        agrupations.push(grupoIndividualVirtual);
-                        agrupations_dao.loadAgrupations(catalogId, agrupations);
-                        defered.resolve(agrupations_dao);
-                    }
-                    gccService.groupsByUser().then(doOK);    
-                });
-		}
+                  }
+
+                  nodeService.nodosTodos(catalogId).then(doOKNodes);
+                }
+                gccService.groupsByUser().then(doOKGroups);
+            });
+		    }
         
         function getAgrupationsByType(catalogId, type){
             return agrupations_dao.getAgrupationsByType(catalogId, type);
@@ -115,8 +131,8 @@
         
         ///////////////////////////////////////// Private \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
         
-        function formatAgrupations(agrupationsFromServer){
-            return agrupationsFromServer.map(function(g){g.type = agrupationTypeVAL.TYPE_GROUP; return g;});
+        function formatAgrupations(agrupationsFromServer, type){
+            return agrupationsFromServer.map(function(g){g.type = type; return g;});
         }
         
         
@@ -160,7 +176,7 @@
                 function(defered){
                     function doOK(response) {					
                         vm.ls.lastUpdate = moment();	
-                        var agrupations = formatAgrupations(response.data);
+                        var agrupations = formatAgrupations(response.data, agrupationTypeVAL.TYPE_GROUP);
                         agrupations_dao.loadAgrupations(catalogId, agrupations);
                         defered.resolve(agrupations_dao);
                     }
