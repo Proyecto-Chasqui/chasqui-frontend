4aebb6e95bd4837d208510aa5e29fb7279b0ab5d
Juan <juan.93.ctm@gmail.com>
2019-10-16 14:35:59 -0300

    Se muestra y oculta el detalle de pedidos de los miembros del nodo

--- src/app/comercio/administracion/nodes-tab/nodesList.ctrl.js
+++ src/app/comercio/administracion/nodes-tab/nodesList.ctrl.js
@@ -8,13 +8,14 @@
     // Interfaz
 
     $scope.newNode = newNode;
-
+    $scope.showOptions = [];
+    $scope.showOptionsForNode = showOptionsForNode;
+    $scope.isLoggedUserNodeAdmin = isLoggedUserNodeAdmin;
 
     // Implementación
 
     function newNode(){
-
-      function doOk(newGroup){
+      function doOk(newNode){
         contextPurchaseService.refresh().then(function(){
           newGroupCreated = true;
           $rootScope.$emit('new-node');
@@ -24,11 +25,23 @@
       dialogCommons.newNode(doOk);
     }
 
+    function showOptionsForNode(nodeIndex){
+      $scope.showOptions = $scope.showOptions.map(function(o,i){return i == nodeIndex && !o});
+      console.log($scope.showOptions);
+    }
+
+    function isLoggedUserNodeAdmin(node){
+      return node.esAdministrador;
+    }
+
 
     // Inicialización
     function init(){
-        
+      $scope.showOptions = $scope.nodes.map(function(n){return false});
+      console.log($scope.showOptions);
     }
+
+    $rootScope.$on('nodes-are-loaded', init);
 
     init();
 
