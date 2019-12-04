70a6cf7795230725373d13d45c9aec0e0b110026
Juan <juan.93.ctm@gmail.com>
2019-12-04 12:45:47 -0300

    Agregados los filtros a la homepage

--- src/app/comercio/principal/home/home.ctrl.js
+++ src/app/comercio/principal/home/home.ctrl.js
@@ -6,27 +6,75 @@
 		.controller('HomeController', HomeController);
 
     
-	function HomeController($scope, URLS, contextCatalogsService, $state) {
+	function HomeController($scope, URLS, contextCatalogsService, $state, sellerService) {
         
-        $scope.catalogs = [];
-        $scope.url = url;
-        $scope.goToCatalog = goToCatalog;
+    $scope.catalogs = [];
+    $scope.url = url;
+    $scope.goToCatalog = goToCatalog;
         
+
+    ////////////////////////////////////
+
+    // busqueda de catalogos
+
+    $scope.tags = {};
+    $scope.selectedTags = {
+      idTagTipoOrganizacion: 0,
+      idTagTipoProducto: 0,
+      idTagZonaDeCobertura: 0,
+      nombre: "",
+      usaEstrategiaNodos: false,
+      usaEstrategiaGrupos: false,
+      usaEstrategiaIndividual: false,
+      entregaADomicilio: false,
+      usaPuntoDeRetiro: false
+    }
+
+    $scope.confirmSearch = function (){
+      // adaptacion
+
+      const selectedTags = {
+        nombre: $scope.selectedTags.nombre,
+        usaEstrategiaNodos: $scope.selectedTags.usaEstrategiaNodos,
+        usaEstrategiaGrupos: $scope.selectedTags.usaEstrategiaGrupos,
+        usaEstrategiaIndividual: $scope.selectedTags.usaEstrategiaIndividual,
+        entregaADomicilio: $scope.selectedTags.entregaADomicilio,
+        usaPuntoDeRetiro: $scope.selectedTags.usaPuntoDeRetiro,
+      }
+      selectedTags.idsTagsTipoOrganizacion = $scope.selectedTags.idTagTipoOrganizacion == 0? [] : [$scope.selectedTags.idTagTipoOrganizacion];
+      selectedTags.idsTagsTipoProducto = $scope.selectedTags.idTagTipoProducto == 0? [] : [$scope.selectedTags.idTagTipoProducto];
+      selectedTags.idsTagsZonaDeCobertura = $scope.selectedTags.idTagZonaDeCobertura == 0? [] : [$scope.selectedTags.idTagZonaDeCobertura];
+
+      console.log(selectedTags);
+
+      sellerService.getSellersWithTags(selectedTags).then(function(response){
+        $scope.tags = response.data;
+        console.log($scope.tags);
+      })
+
+    }
+
+    ////////////////////////////////////
+
         
-        function url(path){
-            return URLS.be_base + path;
-        }
-        
-        function goToCatalog(catalogShortName){
-            $state.go('catalog.landingPage', {catalogShortName: catalogShortName});
-        }
-        
-        function init(){
-            contextCatalogsService.getCatalogs().then(function(catalogs){
-                $scope.catalogs = catalogs;
-            })
-        }
-        
-        init();
+    function url(path){
+        return URLS.be_base + path;
+    }
+    
+    function goToCatalog(catalogShortName){
+        $state.go('catalog.landingPage', {catalogShortName: catalogShortName});
+    }
+    
+    function init(){
+      contextCatalogsService.getCatalogs().then(function(catalogs){
+          $scope.catalogs = catalogs;
+      })
+      sellerService.getSellersTags().then(function(response){
+        $scope.tags = response.data;
+        console.log($scope.tags);
+      })
+    }
+    
+    init();
 	}
 })();
