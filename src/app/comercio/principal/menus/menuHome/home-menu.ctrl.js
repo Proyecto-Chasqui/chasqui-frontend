60e5ac4ed15b31084e14e3fc987c3e8ab7f059e3
Juan <juan.93.ctm@gmail.com>
2019-12-09 14:39:19 -0300

    Agregado el menu de multicatalogo

--- src/app/comercio/principal/menus/menuHome/home-menu.ctrl.js
+++ src/app/comercio/principal/menus/menuHome/home-menu.ctrl.js
@@ -6,7 +6,30 @@
 		.controller('HomeMenuController', HomeMenuController);
 
     
-	function HomeMenuController() {
+	function HomeMenuController(URLS, $scope, usuario_dao) {
+
+    $scope.urlBase = URLS.be_base;
+    $scope.urlLogo = "assets/images/chasqui_logo.png";
         
+    
+    $scope.logOut = function() {
+      $log.debug("Log Out ..... ");
+
+      usuario_dao.logOut();
+      contextPurchaseService.clean();
+
+      $interval.cancel(llamadoPeriodico);
+
+      init();
+      $scope.$broadcast('logout');
+      $state.go('catalog.landingPage');
+    }
+
+    function init() {
+      $scope.usuario = usuario_dao.getUsuario();
+      $scope.isLogued = usuario_dao.isLogged();
+    }
+
+    init();
 	}
 })();
