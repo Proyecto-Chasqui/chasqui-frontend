3735feb129eb213e43c3bde7d0d00081d60bb72e
Juan <juan.93.ctm@gmail.com>
2020-01-24 16:23:04 -0300

    Validadas las preguntas de consumo

--- src/app/comercio/carrito/confirm-order/answer-seller-questions/answer-seller-questions.ctrl.js
+++ src/app/comercio/carrito/confirm-order/answer-seller-questions/answer-seller-questions.ctrl.js
@@ -4,70 +4,56 @@
 	angular.module('chasqui').controller('AnswerSellerQuestionsController', AnswerSellerQuestionsController);
 
 	/** @ngInject */
-	function AnswerSellerQuestionsController($scope, $rootScope, $stateParams, sellerService, $log) {
-        
-        $scope.questions = [];
-        
-        /////////////////////////////////////
-        
-        function init(){
-            
-            function doOk(response){
-                $scope.questions = response.data;
-            }
-            
-            var getQuestions = {
-                PERSONAL: function(){
-                    sellerService.getSellerIndividualQuestions($stateParams.catalogShortName).then(doOk);                    
-                },
-                GROUP: function(){
-                    sellerService.getSellerColectiveQuestions($stateParams.catalogShortName).then(doOk);
-                },
-                NODE: function(){
-                    sellerService.getSellerColectiveQuestions($stateParams.catalogShortName).then(doOk);
-                }
-            }[$scope.order.type]();
-        }
-
-
-        // $rootScope.$on('order-loaded-suc', function(event, order){
-        //   console.log("order-loaded-suc 1", order);
-        //   $scope.order = order;
-        //   init();
-        // });
+	function AnswerSellerQuestionsController($scope, $stateParams, sellerService) {
+      
+    $scope.questions = [];
+    $scope.validateAndNext = validateAndNext;
     
-        init();
+    /////////////////////////////////////
+      
+    function init(){
         
-        /////////////////////////////////////
-               
-        function validInformation(){
-            return $scope.questions.reduce(function(r,q){return r && (q.answer != null || q.answer != undefined)}, true);
-        }
-        
-        function riseErrors(){
-            $scope.validated = true;
-            $scope.questions = $scope.questions.map(function(q){q.answered = q.answer != null || q.answer != undefined; return q});
-            $log.debug($scope.questions);
-        }
-        
-        $scope.$on("check-answers", function(){
-            if(validInformation()){
-                $log.debug($scope.questions);
-                $scope.validated = false;
-                $scope.okAction($scope.questions);
-            }else{
-                riseErrors();
-            }
-        })
-        
-        $scope.$on("check-answers-length", function(){
+      function doOk(response){
+          $scope.questions = response.data;
           if($scope.questions.length == 0){
-            $scope.okAction($scope.questions);
+            $scope.next($scope.questions);
           }
-        })
+      }
+      
+      var getQuestions = {
+          PERSONAL: function(){
+              sellerService.getSellerIndividualQuestions($stateParams.catalogShortName).then(doOk);                    
+          },
+          GROUP: function(){
+              sellerService.getSellerColectiveQuestions($stateParams.catalogShortName).then(doOk);
+          },
+          NODE: function(){
+              sellerService.getSellerColectiveQuestions($stateParams.catalogShortName).then(doOk);
+          }
+      }[$scope.order.type]();
+  }
 
-        
-        
+
+  // $rootScope.$on('order-loaded-suc', function(event, order){
+  //   console.log("order-loaded-suc 1", order);
+  //   $scope.order = order;
+  //   init();
+  // });
+
+  init();
+  
+  /////////////////////////////////////
+          
+  function validateAndNext(){
+    $scope.validated = true;
+    if(validInformation()){
+      $scope.next($scope.questions);
+    }
+  }
+
+  function validInformation(){
+      return $scope.questions.reduce(function(r,q){return r && (q.answer != null || q.answer != undefined)}, true);
+  }
 	}
 
 })();
