b7a26fe9f3d7c8868b5132f8b464b890c279808f
Leonardo Pavanello <leopavan@gmail.com>
2021-06-15 11:32:38 -0300

    arreglo aceptar o rechazar invitacion a nodo

--- src/app/comercio/administracion/perfil.controller.js
+++ src/app/comercio/administracion/perfil.controller.js
@@ -6,7 +6,7 @@
     
 	function PerfilController($log, $scope, $rootScope,
 		$mdDialog, ToastCommons, toastr, $stateParams, perfilService,
-		gccService,us,contextPurchaseService, usuario_dao, navigation_state) {
+		gccService,nodeService, us,contextPurchaseService, usuario_dao, navigation_state) {
             
 		$log.debug("Init PerfilController ....");
 
@@ -98,7 +98,12 @@
 			var params = {};
 			params.idInvitacion = notificacion.id;
 
-			gccService.aceptarInvitacionAGrupo(params).then(doOk)
+            if(vm.isInvitacionANodo(notificacion)) {
+                nodeService.acceptInvitation(params).then(doOk)
+            } else {
+                gccService.aceptarInvitacionAGrupo(params).then(doOk)
+            }
+
 
 		}
 
@@ -110,8 +115,11 @@
 			var params = {};
 			params.idInvitacion = notificacion.id;
 
-			gccService.rechazarInvitacionAGrupo(params).then(doOk)
-
+            if(vm.isInvitacionANodo(notificacion)) { 
+                nodeService.declineInvitation(params).then(doOk);
+            } else {
+                gccService.rechazarInvitacionAGrupo(params).then(doOk);
+            }
 		}
 
 
@@ -168,6 +176,11 @@
                 return false;
             }
             return vm.isInvitacion(notificacion);
+        }
+
+        vm.isInvitacionANodo = function(notificacion) {
+            var mensaje = notificacion.mensaje.toLowerCase();
+            return mensaje.indexOf("te ha invitado al nodo") > -1 && mensaje.indexOf("compras colectivas") > -1;
         }
 
     vm.getColor = function(notificacion){
